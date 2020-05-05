cbd3927a9ae93b608f128922ae28c28e53a11cb8
ritikc31 <ritikchaurasia5@gmail.com>
2020-05-05 13:11:28 +0530

    bean creation using coonstructor arguments

--- spring/src/main/java/com/ritik/spring/main/App.java
+++ spring/src/main/java/com/ritik/spring/main/App.java
@@ -11,29 +11,23 @@ public class App
 {
     public static void main( String[] args )
     {
-//    	retrieving using FileSystemXmlApplicationContext
-//        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/com/ritik/spring/person/beans.xml");
-        
+    	
 //    	retrieving using ClassPathXmlApplicationContext
         ApplicationContext context = new ClassPathXmlApplicationContext("com/ritik/spring/person/beans.xml");
         
         
         
         //getting an object by bean id
-        Person person = (Person) context.getBean("person");
-        person.speak();
+        Person person1 = (Person) context.getBean("person1");
+        System.out.println(person1);
         
-        //getting an object by type
-        Person person1 = context.getBean(Person.class);
-        person1.speak();
+        Person person2 = (Person) context.getBean("person2");
+        System.out.println(person2);
         
-        //returns total number of beans defined
-        System.out.println(context.getBeanDefinitionCount());
+        Person person3 = (Person) context.getBean("person3");
+        System.out.println(person3);
         
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        for(String name : beanDefinitionNames) {
-        	System.out.print(name+", ");
-        }
+
         
 //        cast it to abstract class AbstractApplicationContext to call close method independent of child class
         ((AbstractApplicationContext)context).close();
--- spring/src/main/java/com/ritik/spring/person/Person.java
+++ spring/src/main/java/com/ritik/spring/person/Person.java
@@ -1,7 +1,25 @@
 package com.ritik.spring.person;
 
 public class Person {
+
+	private int id;
+	private String name;
+
+	//for creating bean with no constructor args defined 
+	public Person() {
+	}
+
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
 	public void speak() {
 		System.out.println("Hello...");
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
 	}
 }
