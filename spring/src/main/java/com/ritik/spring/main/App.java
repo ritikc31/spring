5b8fd31c7a64b58706ef8375cecb66e2205b95a1
ritikc31 <ritikchaurasia5@gmail.com>
2020-05-08 17:50:31 +0530

    setting list propeties

--- spring/src/main/java/com/ritik/spring/main/App.java
+++ spring/src/main/java/com/ritik/spring/main/App.java
@@ -3,40 +3,25 @@ package com.ritik.spring.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.ritik.spring.person.Person;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-//    	retrieving using FileSystemXmlApplicationContext
-//        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/com/ritik/spring/person/beans.xml");
-        
+public class App {
+	public static void main(String[] args) {
+
 //    	retrieving using ClassPathXmlApplicationContext
-        ApplicationContext context = new ClassPathXmlApplicationContext("com/ritik/spring/person/beans.xml");
-        
-        
-        
-        //getting an object by bean id
-        Person person = (Person) context.getBean("person");
-        person.speak();
-        
-        //getting an object by type
-        Person person1 = context.getBean(Person.class);
-        person1.speak();
-        
-        //returns total number of beans defined
-        System.out.println(context.getBeanDefinitionCount());
-        
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        for(String name : beanDefinitionNames) {
-        	System.out.print(name+", ");
-        }
-        
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/ritik/spring/person/beans.xml");
+
+		// getting an object by bean id
+		Person person1 = (Person) context.getBean("person1");
+		System.out.println(person1);
+		
+		// getting an object by bean id
+		Person person2 = (Person) context.getBean("person2");
+		System.out.println(person2);
+
 //        cast it to abstract class AbstractApplicationContext to call close method independent of child class
-        ((AbstractApplicationContext)context).close();
-        
-    }
+		((AbstractApplicationContext) context).close();
+
+	}
 }
--- spring/src/main/java/com/ritik/spring/person/Person.java
+++ spring/src/main/java/com/ritik/spring/person/Person.java
@@ -1,7 +1,36 @@
 package com.ritik.spring.person;
 
+import java.util.List;
+
 public class Person {
-	public void speak() {
-		System.out.println("Hello...");
+	private String name;
+	private List<String> favouritFruits;
+
+	public Person(String name, List<String> favouritFruits) {
+		super();
+		this.name = name;
+		this.favouritFruits = favouritFruits;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getFavouritFruits() {
+		return favouritFruits;
+	}
+
+	public void setFavouritFruits(List<String> favouritFruits) {
+		this.favouritFruits = favouritFruits;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", favouritFruits=" + favouritFruits + "]";
+	}
+
 }
