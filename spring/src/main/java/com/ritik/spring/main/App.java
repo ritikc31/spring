bd3dcdaa6a3d28b5edfd1a38c7171b5e6c8a6f00
ritikc31 <ritikchaurasia5@gmail.com>
2020-05-08 14:22:12 +0530

    bean scope singleton and prototype

--- spring/src/main/java/com/ritik/spring/main/App.java
+++ spring/src/main/java/com/ritik/spring/main/App.java
@@ -3,37 +3,35 @@ package com.ritik.spring.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.ritik.spring.person.Person;
 
+/*
+ * after running once change the bean scope to prototype to see difference.
+ * spring will return the different object of the bean every time when scope is prototype.
+ */
 public class App 
 {
     public static void main( String[] args )
-    {
-//    	retrieving using FileSystemXmlApplicationContext
-//        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/com/ritik/spring/person/beans.xml");
-        
+    {   
 //    	retrieving using ClassPathXmlApplicationContext
         ApplicationContext context = new ClassPathXmlApplicationContext("com/ritik/spring/person/beans.xml");
+                
+		/*
+		 * getting an object by bean id 
+		 * by default scope is singleton
+		 */
+        Person person1 = (Person) context.getBean("person");
+        System.out.println("Initial state: "+person1);
         
+		/* change the name of person1 */
+        person1.setName("Eric");
+        System.out.println("After changing name :"+person1);
         
+		/* get the bean with same id */
+        Person person2 = (Person) context.getBean("person");
+        System.out.println("person2 :"+person2);
         
-        //getting an object by bean id
-        Person person = (Person) context.getBean("person");
-        person.speak();
-        
-        //getting an object by type
-        Person person1 = context.getBean(Person.class);
-        person1.speak();
-        
-        //returns total number of beans defined
-        System.out.println(context.getBeanDefinitionCount());
-        
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        for(String name : beanDefinitionNames) {
-        	System.out.print(name+", ");
-        }
         
 //        cast it to abstract class AbstractApplicationContext to call close method independent of child class
         ((AbstractApplicationContext)context).close();
--- spring/src/main/java/com/ritik/spring/person/Person.java
+++ spring/src/main/java/com/ritik/spring/person/Person.java
@@ -1,7 +1,23 @@
 package com.ritik.spring.person;
 
 public class Person {
+	private String name;
+	
 	public void speak() {
 		System.out.println("Hello...");
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+	
 }
